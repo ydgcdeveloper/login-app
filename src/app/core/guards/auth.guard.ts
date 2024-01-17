fc40334b4a6ddedb5e22710bb71884a35329d0c6
ydgcdeveloper <ydgcdeveloper@gmail.com>
2024-01-17 16:51:44 -0500

    set guard for authentication

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,12 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  return inject(AuthService).isAuthenticated() ? true : inject(Router).createUrlTree(['/login']);
+};
+
+
+export const loggedInGuard: CanActivateFn = (route, state) => {
+  return inject(AuthService).isAuthenticated() ? inject(Router).createUrlTree(['/']) : true;
+};
