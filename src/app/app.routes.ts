7500d3992b4e641bcdeabf2d463ba64e9f2ef36e
ydgcdeveloper <ydgcdeveloper@gmail.com>
2024-01-17 16:50:51 -0500

    set app routes and guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,20 @@
 import { Routes } from '@angular/router';
+import { authGuard, loggedInGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
   },
   {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    canActivate: [authGuard]
+  },
+  {
     path: 'login',
-    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage),
+    canActivate: [loggedInGuard]
   },
 ];
